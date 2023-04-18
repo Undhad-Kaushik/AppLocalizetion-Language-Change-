085522a9a3b0205a11b7636570f21ca97b4547d9
Undhad Kaushik <kpkushpatel8@gmail.com>
2023-04-18 09:01:28 +0530

    Initial Commit

---
+++ AppLocalizetion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLocalizetion
+//
+//  Created by undhad kaushik on 18/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
